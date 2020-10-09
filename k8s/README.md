641e48199e1cba3d486de0f2ff4426c25a05aa19
Jason-ZW <zhenyang@rancher.com>
2020-10-09 13:33:49 +0800

    feat(k8s): add install kubernetes scripts
    Signed-off-by: Jason-ZW <zhenyang@rancher.com>

--- k8s/README.md
+++ k8s/README.md
@@ -1,1 +1,18 @@
-# kube-kata-poc
+# k8s中部署kata
+
+## 安装containerd
+```bash
+./k8s/containerd/containerd.sh
+```
+
+## 安装kubeadm及kubernetes
+```bash
+./k8s/kubeadm/kubeadm.sh
+```
+
+## 部署kata-deploy
+```bash
+kubectl apply -k k8s/rbac/base/
+kubectl apply -k k8s/main/overlays/k8s/
+kubectl apply -f k3s/runtime/
+```
---
+++ k8s/kubeadm/kubeadm.sh
@@ -0,0 +1,90 @@
+#!/bin/sh
+set -e
+
+BIN_DIR=/usr/bin
+
+# --- helper functions for logs ---
+info()
+{
+    echo '[INFO] ' "$@"
+}
+warn()
+{
+    echo '[WARN] ' "$@" >&2
+}
+fatal()
+{
+    echo '[ERROR] ' "$@" >&2
+    exit 1
+}
+
+# --- fatal if no systemd or openrc ---
+verify_system() {
+    if [ -d /run/systemd ]; then
+        HAS_SYSTEMD=true
+        return
+    fi
+    fatal 'Can not find systemd to use as a process supervisor for kubernetes'
+}
+
+# --- set arch and suffix, fatal if architecture not supported ---
+setup_verify_arch() {
+    ARCH=$(uname -m)
+    case $ARCH in
+        amd64)
+            ARCH=amd64
+            SUFFIX=
+            ;;
+        x86_64)
+            ARCH=amd64
+            SUFFIX=
+            ;;
+        *)
+            fatal "Unsupported architecture $ARCH"
+    esac
+}
+
+
+# --- setup permissions and move binary to system directory ---
+setup_binary() {
+    info "Installing kubeadm to ${BIN_DIR}"
+    $SUDO apt-get update && sudo apt-get install -y apt-transport-https curl
+    $SUDO curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | $SUDO apt-key add -
+    $SUDO cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+    $SUDO apt-get update && $SUDO apt-get install -y kubelet kubeadm kubectl && $SUDO apt-mark hold kubelet kubeadm kubectl
+    verify_kubeadm_is_executable
+    $SUDO mkdir -p  /etc/systemd/system/kubelet.service.d/
+    $SUDO cat << EOF | sudo tee /etc/systemd/system/kubelet.service.d/0-containerd.conf
+[Service]
+Environment="KUBELET_EXTRA_ARGS=--container-runtime=remote --runtime-request-timeout=15m --container-runtime-endpoint=unix:///run/containerd/containerd.sock"
+EOF
+    $SUDO systemctl daemon-reload
+    $SUDO kubeadm init --cri-socket /run/containerd/containerd.sock --pod-network-cidr=10.244.0.0/16
+    $SUDO mkdir -p $HOME/.kube && $SUDO cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && $SUDO chown $(id -u):$(id -g) $HOME/.kube/config
+    $SUDO curl -o kube-flannel.yml -sfL https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+    $SUDO sed 's/vxlan/host-gw/' -i kube-flannel.yml
+    kubectl apply -f kube-flannel.yml
+    kubectl taint nodes --all node-role.kubernetes.io/master-
+}
+
+# --- verify an executabe kubeadm binary is installed ---
+verify_kubeadm_is_executable() {
+    if [ ! -x ${BIN_DIR}/kubeadm ]; then
+        fatal "Executable kubeadm binary not found at ${BIN_DIR}/kubeadm"
+    fi
+}
+
+
+# --- download and verify kubeadm ---
+download_and_verify() {
+    setup_verify_arch
+    setup_binary
+}
+
+# --- run the install process --
+{
+    verify_system
+    download_and_verify
+}
